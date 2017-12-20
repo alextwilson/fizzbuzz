0c59430e746eb406a2b970466d38f564784b500a
Alex Wilson <github@alextwilson.co.uk>
2017-12-20 23:13:00 +0000

    Returns 'fizzbuzz' when passed 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number == 15
+    return 'fizzbuzz'
+  elsif number % 3 == 0
     return 'fizz'
   elsif number == 5 || number == 10
     return 'buzz'
