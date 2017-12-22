b9b1a0fa87283fddbc3fe8e42ab04394a19abd01
Alex Wilson <github@alextwilson.co.uk>
2017-12-22 15:13:24 +0000

    Retuns buzz for any multiple of 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 public def fizzbuzz
   if self % 3 == 0
     return "fizz"
-  elsif self == 5
+  elsif self % 5 == 0
     return "buzz"
   else
     return self.to_s
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,4 +24,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" for the number 6' do
     expect(6.fizzbuzz).to eq 'fizz'
   end
+
+  it 'returns "buzz" for the number 10' do
+    expect(10.fizzbuzz).to eq 'buzz'
+  end
 end
